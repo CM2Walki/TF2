e8c1f6af7bd52a1a5a31da5208581664eae85f1a
CM2Walki <walentinlamonos@googlemail.com>
2021-12-13 00:18:31 +0100

    Swapped steamcmd login & force_install_dir order to follow new standard

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p "${STEAMAPPDIR}" || true  
 
-bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous \
-				+force_install_dir "${STEAMAPPDIR}" \
+bash "${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" \
+				+login anonymous \
 				+app_update "${STEAMAPPID}" \
 				+quit
 
